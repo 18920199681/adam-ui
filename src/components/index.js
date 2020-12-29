3fd7a4bb45ccaf548d431e087de84f71b5553ca5
杨国栋 <v_guodong.yang@fowo.com>
2020-12-29 17:58:26 +0800

    工具组件及模板组件区分

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,19 @@
-import gdInput from './Input';
+// import gdInput from './Input';
 
-const components = [
-  gdInput,
-];
+// const components = [
+//   gdInput,
+// ];
 
-const install = function (Vue) {
-  if (install.installed) return;
-  components.map(item => Vue.component(item.name, item));
-};
+// const install = function (Vue) {
+//   if (install.installed) return;
+//   components.map(item => Vue.component(item.name, item));
+// };
 
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
-}
+// if (typeof window !== 'undefined' && window.Vue) {
+//   install(window.Vue);
+// }
 
-export default {
-  install,
-  gdInput,
-};
+// export default {
+//   install,
+//   gdInput,
+// };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-import Examples from './components/index';
 
+
+/* 工具组件 */
 import Toast from './components/Toast/index';
 Vue.use(Toast);
+
+
+/* 模板组件 */
+import gdInput from './components/Input';
+
+const components = [
+  gdInput,
+];
+
+const install = function (Vue) {
+  if (install.installed) return;
+  components.map(item => Vue.component(item.name, item));
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+export default {
+  install,
+  gdInput,
+};
+
+
 
 Vue.config.productionTip = false;
 
@@ -14,8 +39,3 @@ new Vue({
   components: { App },
   template: '<App/>'
 });
-
-export default {
-  Examples,
-  Toast
-};
